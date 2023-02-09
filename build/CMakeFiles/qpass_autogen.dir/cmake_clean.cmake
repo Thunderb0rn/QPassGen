cb38ec78b5a979bb4b6702bf041c69766d87af9b
Thunderb0rn <110599408+Thunderb0rn@users.noreply.github.com>
2023-02-09 18:38:38 +0300

    Delete build directory

--- build/CMakeFiles/qpass_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/qpass_autogen"
-  "qpass_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/qpass_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
